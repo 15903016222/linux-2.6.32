d68c1c1f67fe6638c8c9432cad704b79b544cd6e
王 胜 <wangsheng@cndoppler.cn>
2017-03-14 15:58:28 +0800

    通过make menuconfig修改 Device Drivers -> USB support ->  USB Gadget Support -> USB Gadget Drivers 设置成Y, 并选择Mass Storage Gadget

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ crossPath="/stuff/bitbake/bin/tmp/sysroots/i686-linux/usr/armv7a/bin/arm-angstro
 if [ ! -f "./.config" ]; then
 	cp ./configs/uImage.config ./.config
 	if [ -f "./.config" ]; then
-		make CROSS_COMPILE=$crossPath  ARCH=arm uImage
+		make CROSS_COMPILE=$crossPath  ARCH=arm uImage -j4
 	else 
 		echo "No .config file."
 	fi
 else 
-	make CROSS_COMPILE=$crossPath  ARCH=arm uImage
+	make CROSS_COMPILE=$crossPath  ARCH=arm uImage -j4
 fi
 
---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Tue Mar 14 15:57:55 CST 2017"
+#define LINUX_COMPILE_TIME "15:57:55"
+#define LINUX_COMPILE_BY "ws"
+#define LINUX_COMPILE_HOST "ws"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.3 20110311 (prerelease) (GCC) "
